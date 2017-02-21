0565a7894702c91561d07691cd8908e412f6e067
liuzhangfu <liuzhangfu@yeah.net>
2017-02-21 22:14:35 +0800

    项目入口文件

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * 项目单入口文件
+ *
+ * @author 		Zorz Dev Team
+ * @copyright 	Copyright (c) 2012 - 2017, Zorz, Inc. (http://www.zorz.cn/)
+ * @license		http://opensource.org/licenses/MIT	MIT License
+ * @link 		http://www.zorz.cn/
+ * @since		Version 1.0.0
+ */
