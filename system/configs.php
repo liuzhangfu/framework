c9b21b5cf67c470914fef72e4a932a4d58b62f4a
liuzhangfu <liuzhangfu@yeah.net>
2018-06-20 14:08:42 +0800

    Create configs.php

---
+++ system/configs.php
@@ -0,0 +1,12 @@
+<?php defined('BASE_PATH') or exit('Access Denied !');
+/**
+ * 框架默认配置
+ *
+ * 定义基本常量及引入框架并执行
+ *
+ * @author 		Zorz Dev Team
+ * @copyright 	Copyright (c) 2012 - 2017, Zorz, Inc. (http://www.zorz.cn/)
+ * @license		http://opensource.org/licenses/MIT	MIT License
+ * @link 		http://www.zorz.cn/
+ * @since		Version 1.0.0
+ */
