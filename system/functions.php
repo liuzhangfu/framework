c9434f2d9b2922072ca70694bc485c13606d675b
liuzhangfu <liuzhangfu@yeah.net>
2018-06-20 14:07:28 +0800

    Create functions.php

---
+++ system/functions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 框架核心函数库
+ *
+ * 定义基本常量及引入框架并执行
+ *
+ * @author 		Zorz Dev Team
+ * @copyright 	Copyright (c) 2012 - 2017, Zorz, Inc. (http://www.zorz.cn/)
+ * @license		http://opensource.org/licenses/MIT	MIT License
+ * @link 		http://www.zorz.cn/
+ * @since		Version 1.0.0
+ */
